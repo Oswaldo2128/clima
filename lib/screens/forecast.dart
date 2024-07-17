abd30757be39f225fb7e456ab14336a5a0e16cf7
Oswaldo Munguia Reyes <crip.damcad018@conahcyt.mx>
2024-07-17 13:47:21 -0600

    feat: agrega estructura para listas

--- lib/screens/forecast.dart
+++ lib/screens/forecast.dart
@@ -5,8 +5,31 @@ class Forecast extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Index 1: Forecast'),
+    final List<String> entries = <String>['A', 'B', 'C'];
+    final List<int> colorCodes = <int>[600, 500, 100];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[colorCodes[index]],
+                child: Center(child: Text('Entry ${entries[index]}')),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          )),
+        ],
+      ),
     );
   }
 }
