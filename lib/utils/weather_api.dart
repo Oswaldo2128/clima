126483c24b570cd072c78ab3e7d0624e38434050
Oswaldo Munguia Reyes <crip.damcad018@conahcyt.mx>
2024-07-17 01:19:00 -0600

    feat: agrega showDialog

--- lib/utils/weather_api.dart
+++ lib/utils/weather_api.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 const apiKey = '1142a9567150c9e455cd4f5ba89953a6';
 
-Future<Map<String, dynamic>?> getWeatherCurrent(double lat, double lon) async {
+Future<Map<String, dynamic>?> getWeatherCurrent(
+    double lat, double lon, BuildContext context) async {
   final url =
       'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&lang=sp&appid=$apiKey&units=metric';
 
@@ -12,11 +14,29 @@ Future<Map<String, dynamic>?> getWeatherCurrent(double lat, double lon) async {
   if (response.statusCode == 200) {
     return json.decode(response.body);
   } else {
-    throw Exception('Error al cargar el clima');
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Alerta"),
+          content: const Text("Error, intente mas tarde"),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+    return null;
   }
 }
 
-Future<Map<String, dynamic>?> getWeatherOfCity(String cityName) async {
+Future<Map<String, dynamic>?> getWeatherOfCity(
+    String cityName, BuildContext context) async {
   final url =
       'http://api.openweathermap.org/data/2.5/weather?q=$cityName&lang=sp&appid=$apiKey&units=metric';
 
@@ -25,6 +45,23 @@ Future<Map<String, dynamic>?> getWeatherOfCity(String cityName) async {
   if (response.statusCode == 200) {
     return json.decode(response.body);
   } else {
-    throw Exception('Error al cargar el clima');
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Alerta"),
+          content: const Text("Error, intente mas tarde"),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
+  return null;
 }
