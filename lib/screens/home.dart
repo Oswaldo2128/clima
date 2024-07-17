85ebe1cd7f59c9cbef23cd862bc8c51799501656
Oswaldo Munguia Reyes <crip.damcad018@conahcyt.mx>
2024-07-17 00:56:30 -0600

    feat: agrega estructura y estilo

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -23,17 +23,42 @@ class Home extends StatelessWidget {
                 'https://openweathermap.org/img/wn/${weatherData?['weather'][0]['icon']}@2x.png')
             : const Text('Cargando...'),
         Text('${weatherData?['main']['temp'] ?? 'Cargando...'} °C'),
+        const SizedBox(height: 5.0),
         Text(
             'Sensación térmica: ${weatherData?['main']['feels_like'] ?? 'Cargando...'} °C'),
-        Text(
-            'Presión: ${weatherData?['main']['pressure'] ?? 'Cargando...'} hPa'),
-        Text('Humedad: ${weatherData?['main']['humidity'] ?? 'Cargando...'} %'),
-        Text(
-            'Visibilidad: ${weatherData?['visibility'] / 1000 ?? 'Cargando...'} km'),
-        Text(
-            'Velocidad: ${weatherData?['wind']['speed'] ?? 'Cargando...'} m/s'),
-        Text('País: ${weatherData?['sys']['country'] ?? 'Cargando...'}'),
-        Text('Ciudad: ${weatherData?['name'] ?? 'Cargando...'}'),
+        const SizedBox(height: 5.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                'Presión: ${weatherData?['main']['pressure'] ?? 'Cargando...'} hPa'),
+            const SizedBox(width: 50.0),
+            Text(
+                'Humedad: ${weatherData?['main']['humidity'] ?? 'Cargando...'} %'),
+          ],
+        ),
+        const SizedBox(height: 5.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.remove_red_eye_outlined,
+              color: Color.fromARGB(255, 0, 0, 0),
+              size: 30.0,
+            ),
+            Text('${weatherData?['visibility'] / 1000 ?? 'Cargando...'} km'),
+            const SizedBox(width: 50.0),
+            const Icon(
+              Icons.speed,
+              color: Color.fromARGB(255, 0, 0, 0),
+              size: 30.0,
+            ),
+            Text('${weatherData?['wind']['speed'] ?? 'Cargando...'} m/s'),
+          ],
+        ),
+        const SizedBox(height: 5.0),
+        Text('${weatherData?['name'] ?? 'Cargando...'}'
+            ', ${weatherData?['sys']['country'] ?? 'Cargando...'}'),
       ],
     );
   }
